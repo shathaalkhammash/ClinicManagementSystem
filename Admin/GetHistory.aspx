f5a7c16755b8f3443bfabc2aec4868a21bba0a91
shathakh <148899480+shathaalkhammash@users.noreply.github.com>
2026-03-26 23:48:36 +0300

    Add files via upload

---
+++ Admin/GetHistory.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="GetHistory.aspx.cs" Inherits="shatha11822App3.ClinicManagementSystem.Admin.GetHistory" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ Admin/GetHistory.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.UI;
+
+namespace shatha11822App3.ClinicManagementSystem.Admin
+{
+    public partial class GetHistory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string pid = Request.QueryString["pid"];
+            string connStr = ConfigurationManager.ConnectionStrings["ClinicDB"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                
+                string sql = @"SELECT appointmentDate, Notes 
+                       FROM Appointments 
+                       WHERE patientId = @pid AND Notes IS NOT NULL 
+                       ORDER BY appointmentDate DESC";
+
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@pid", pid);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                string html = "<ul class='list-group list-group-flush'>";
+
+                bool hasHistory = false;
+                while (rdr.Read())
+                {
+                    hasHistory = true;
+                    string date = Convert.ToDateTime(rdr["appointmentDate"]).ToString("MMM dd, yyyy");
+                    string note = rdr["Notes"].ToString();
+
+                    html += $"<li class='list-group-item'>" +
+                            $"<small class='text-muted d-block'>{date}</small>" +
+                            $"<strong>Diagnosis/Illness:</strong> {note}</li>";
+                }
+
+                if (!hasHistory) html += "<li class='list-group-item'>No recorded illness history.</li>";
+
+                html += "</ul>";
+                Response.Write(html);
+                Response.End();
+            }
+        }
+    }
+}
