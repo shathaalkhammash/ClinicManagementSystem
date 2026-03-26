adb66c19368c75a36f656bc63c58f65e5065a5fd
shathakh <148899480+shathaalkhammash@users.noreply.github.com>
2026-03-26 23:38:59 +0300

    Implement dashboard statistics retrieval
    Added functionality to retrieve and display dashboard statistics for patients, surveys, and doctors.

--- Admin/AdminDashboard.aspx.cs
+++ Admin/AdminDashboard.aspx.cs
@@ -1,17 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System;
+using System.Data.SqlClient;
+using System.Configuration;
 
-namespace shatha11822App3.web11.Admin
+namespace shatha11822App3.ClinicManagementSystem.Admin
 {
     public partial class AdminDashboard : System.Web.UI.Page
     {
+        string connStr = ConfigurationManager.ConnectionStrings["ClinicDB"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                GetDashboardStats();
+            }
+        }
 
+        private void GetDashboardStats()
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                con.Open();
+
+                
+                SqlCommand cmd1 = new SqlCommand("SELECT COUNT(*) FROM Patients", con);
+                lblPatientCount.Text = cmd1.ExecuteScalar().ToString();
+
+                
+                SqlCommand cmd2 = new SqlCommand("SELECT COUNT(*) FROM PatientSurveys", con);
+                lblSurveyCount.Text = cmd2.ExecuteScalar().ToString();
+
+               
+                try
+                {
+                    SqlCommand cmd3 = new SqlCommand("SELECT COUNT(*) FROM Doctors", con);
+                    lblDoctorCount.Text = cmd3.ExecuteScalar().ToString();
+                }
+                catch
+                {
+                    lblDoctorCount.Text = "0"; 
+                }
+            }
         }
     }
 }
